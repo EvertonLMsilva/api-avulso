94977f03ad498a753f8566ccbfeeb2f4efcd76ff
Everton LM Silva <everton.l.m.silva@gmail.com>
2024-10-03 16:54:24 -0300

    First commit v1

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-
-	user "github.com/EvertonLMsilva/api-avulso/pkg/controller/user"
 )
 
 func main() {
-	http.HandleFunc("/user/all", user.GetAllUser)
+	http.HandleFunc("/user/all", GetAllUser)
 
 	port := 3000
 	log.Println("Server listennig on $s", port)
